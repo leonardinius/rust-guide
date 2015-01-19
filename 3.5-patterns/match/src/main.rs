e38fcd6405e2b80b2feb8ecf556ba30ab0569f1d
Leonids Maslovs <leonids.maslovs@galeoconsulting.com>
2015-01-19 02:30:29 +0200

    add more pattern samples

--- 3.5-patterns/match/src/main.rs
+++ 3.5-patterns/match/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let x = 5i32;
+    let x = 4;
 
     match x {
         1 | 2  => println!("one or two"),
@@ -10,6 +10,11 @@ fn main() {
 
     match x {
         1...5 => println!("one through five"),
+        _ => println!("anything"),
+    }
+
+    match x {
+        e @ 1...5 => println!("one through five, {}", e),
         _ => println!("anything"),
     }
 }
