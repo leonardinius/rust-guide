0a71cf94ae7abbde3ee12e4e960be64c6690c659
Leonids Maslovs <leonids.maslovs@galeoconsulting.com>
2015-01-19 02:59:12 +0200

    Add slice match example

---
+++ 3.5-patterns/match-slice/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    println!("Hello, world!");
+
+    let v = vec!["match this", "1"];
+
+    match v.as_slice() {
+        ["match this", second] => println!("the second element is {}", second),
+        _ => {},
+    }
+}
