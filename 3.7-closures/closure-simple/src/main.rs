c403e99c6bdcd6f327223e4270c9e45e17b5b137
Leonids Maslovs <leonids.maslovs@galeoconsulting.com>
2015-01-29 08:32:51 +0200

    Simple closure - adding more (breaking) samples (commented out)

--- 3.7-closures/closure-simple/src/main.rs
+++ 3.7-closures/closure-simple/src/main.rs
@@ -3,4 +3,10 @@ fn main() {
 
     let add_one = |&: x| {1 + x };
     println!("The sum of 5 plus is {}", add_one(5));
+
+    let x = &mut 5;
+    let full_closure =|&: | { println!("full closure, x is {}", x); };
+    //*x = 8i32;
+    full_closure();
+    println!("x={}", x);
 }
