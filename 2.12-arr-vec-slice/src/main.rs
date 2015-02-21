34f73812e496b7269a57652c1e04d20404db612b
Leonids Maslovs <leonids.maslovs@galeoconsulting.com>
2015-02-21 22:06:02 +0200

    Fix format trait errors (breaking change)

--- 2.12-arr-vec-slice/src/main.rs
+++ 2.12-arr-vec-slice/src/main.rs
@@ -1,13 +1,16 @@
 fn _12_1(){
     println!("guide 12-1");
 
-    let a = [1i, 2i, 3i];
-    let mut m = [2i, 3i, 4i];
+    let a = [1i32, 2i32, 3i32];
+    let mut m = [2i32, 3i32, 4i32];
+    if false {
+      println!("{:?} {:?}", a, m);
+    }
 
-    let b = [0i, ..20]; // shorthand for array of 20 elements all initialized to 0
-    println!("{}", b);
-    m = [5i, 6i, 7i];
-    println!("{}", m);
+    let b = [0i32; 20]; // shorthand for array of 20 elements all initialized to 0
+    println!("{:?}", b);
+    m = [5i32, 6i32, 7i32];
+    println!("{:?}", m);
 
     for i in m.iter() {
         println!("elem {}", i);
@@ -20,18 +23,18 @@ fn _12_1(){
 fn _12_2(){
     println!("guide 12-2");
 
-    let mut v = vec![1i, 2, 3];
+    let mut v = vec![1i32, 2, 3];
     v.push(4);
-    println!("{}, len is {}", v, v.len());
+    println!("{:?}, len is {}", v, v.len());
 }
 
 fn _12_3(){
     println!("guide 12-3");
 
-    let mut a = vec![0i, 1, 2, 3, 4];
-    let middle = a.as_mut_slice(); 
+    let mut a = vec![0i32, 1, 2, 3, 4];
+    let middle = a.as_mut_slice();
 
-    middle[0] = 10i;
+    middle[0] = 10i32;
 
     for e in middle.iter() {
         println!("{}", e);
