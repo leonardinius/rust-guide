fa15977cff88cb8c7c5997540f842be9b0cc4660
Leonids Maslovs <leonids.maslovs@galeoconsulting.com>
2015-02-21 23:23:31 +0200

    fix formatting (breaking change)

--- 2.8-data-types/src/main.rs
+++ 2.8-data-types/src/main.rs
@@ -1,20 +1,22 @@
+use std::cmp::Ordering;
+
 fn _8_1(){
-    let x1 = ( 1i, "hello");
-    println!("tuple x1 = {}", x1);
+    let x1 = ( 1i32, "hello");
+    println!("tuple x1 = {:?}", x1);
 
-    let x2: (int, &str) = (2, "my world");
-    println!("x2 = {}", x2);
+    let x2: (i32, &str) = (2, "my world");
+    println!("x2 = {:?}", x2);
 
-    let (x, y, z) = (1i, 2i, 3i);
+    let (x, y, z) = (1i32, 2i32, 3i32);
     println!("x = {}, y = {}, z = {}", x, y, z);
 
-    let mut a = (1i, 2i);
-    let b = (2i, 3i);
+    let mut a;
+    let b = (2i32, 3i32);
     a = b;
-    println!("a = b = {}", a);
+    println!("a = b = {:?}", a);
 
-    let q = (1i, 2i, 3i);
-    let w = (2i, 2i, 4i);
+    let q = (1i32, 2i32, 3i32);
+    let w = (2i32, 2i32, 4i32);
 
     if q == w {
             println!("yes");
@@ -24,46 +26,47 @@ fn _8_1(){
 }
 
 struct Point {
-    x: int,
-    y: int,
+    x: i32,
+    y: i32,
 }
 
 fn _8_2() {
-    let origin = Point { x: 0i, y: 0i };
+    let origin = Point { x: 0i32, y: 0i32 };
     println!("The origin is at ({}, {})", origin.x, origin.y);
 
-    let mut o2 = Point { x: 1i, y: 2i };
-    o2.x = 10i;
+    let mut o2 = Point { x: 1i32, y: 2i32 };
+    o2.x = 10i32;
     println!("The origin is at ({}, {})", o2.x, o2.y);
 }
 
-struct Inches(int);
+#[derive(Debug)]
+struct Inches(i32);
 
 fn _8_3(){
     let length = Inches(10);
 
     let Inches(a) = length;
 
-    println!("length is {} {1:X} inches", a, a);
+    println!("length {:?} is {} {:X} inches", length, a, a);
 }
 
-fn cmp(a: int, b: int) -> Ordering {
-    if a < b { Less }
-    else if a > b { Greater }
-    else { Equal }
+fn cmp(a: i32, b: i32) -> Ordering {
+    if a < b { Ordering::Less }
+    else if a > b { Ordering::Greater }
+    else { Ordering::Equal }
 }
 
 fn _8_4() {
-    let x = 5i;
-    let y = 10i;
+    let x = 5i32;
+    let y = 10i32;
 
     let ordering = cmp(x, y);
 
-    if ordering == Less {
+    if ordering == Ordering::Less {
         println!("less");
-    } else if ordering == Greater {
+    } else if ordering == Ordering::Greater {
         println!("greater");
-    } else if ordering == Equal {
+    } else if ordering == Ordering::Equal {
         println!("equal");
     }
 }
