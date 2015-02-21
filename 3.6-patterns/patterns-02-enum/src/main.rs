732a769f02f5c5d3ed42a9249f3dd304e6a439c9
Leonids Maslovs <leonids.maslovs@galeoconsulting.com>
2015-02-21 23:58:28 +0200

    fix int type

--- 3.6-patterns/patterns-02-enum/src/main.rs
+++ 3.6-patterns/patterns-02-enum/src/main.rs
@@ -1,5 +1,5 @@
 enum OptionalInt {
-    Value(int),
+    Value(i32),
     Missing
 }
 
@@ -7,11 +7,19 @@ enum OptionalInt {
 fn main() {
     println!("Hello, world!");
 
-    let x = OptionalInt::Value(15i);
+    let x = OptionalInt::Value(15i32);
 
     match x {
-        OptionalInt::Value(i) if i > 5 => println!("Got an int bigger than five!"),
-        OptionalInt::Value(..) => println!("Got an int!"),
+        OptionalInt::Value(i) if i > 5 => println!("Got an i32 bigger than five!"),
+        OptionalInt::Value(..) => println!("Got an i32!"),
+        OptionalInt::Missing => println!("No such luck"),
+    };
+
+    let y = OptionalInt::Missing;
+
+    match y {
+        OptionalInt::Value(i) if i > 5 => println!("Got an i32 bigger than five!"),
+        OptionalInt::Value(..) => println!("Got an i32!"),
         OptionalInt::Missing => println!("No such luck"),
     };
 }
