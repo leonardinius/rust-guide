b36a65851b773dfa075cf6f92aa89a4878b58738
Leonids Maslovs <leonids.maslovs@galeoconsulting.com>
2015-02-22 00:24:13 +0200

    int type

--- 3.4-pointers/reference_counters/src/main.rs
+++ 3.4-pointers/reference_counters/src/main.rs
@@ -2,8 +2,8 @@ use std::rc::Rc;
 
 fn main() {
     println!("Hello, world!");
-    
-    let x = Rc::new(5i);
+
+    let x = Rc::new(5i32);
     let y = x.clone();
 
     println!("{} {}", *x, *y);
